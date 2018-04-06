bef2a2d054bfd0c2e0c1c2db3d7d42b449c7871e
yawuling <1297640072@qq.com>
2018-04-06 20:22:05 +0800

    Site updated: 2018-04-06 20:22:03

--- js/Ani.js
+++ js/Ani.js
@@ -1,25 +1,35 @@
 var toolbarTop = $('#toolbar').scrollTop(),
   toolbarHeight = $('#toolbar').height();
+
 $(document).ready(function () {
   $('.markdown-body img').each(function () {
     $(this).wrap('<a href="' + this.src + '" data-fancybox="group"></a>')
   });
   $("[data-fancybox]").fancybox();
+
+  $('.markdown-body figure').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
 });
+
 $('.js-transition-hover').hover(function () {
   $(this).find('.js-transition-bottom').addClass('focus');
 }, function(){
   $(this).find('.js-transition-bottom').removeClass('focus');
 });
+
 $('.search-input').focus(function(){
   $(this).parent().find('.js-transition-bottom').addClass('focus');
 });
+
 $('.search-input').blur(function(){
   $(this).parent().find('.js-transition-bottom').removeClass('focus');
 });
+
 $('#to-top').click(function(){
   $('html, body').animate({scrollTop:0}, 'slow');
 });
+
 $(window).scroll(function () {
   var scrollTop = $(window).scrollTop();
 
@@ -34,7 +44,13 @@ $(window).scroll(function () {
     $('#post-toc').removeClass('fixed');
   }
 });
+
 $('#tip-btn').on('click', function () {
   var tipBlock = $(this).next('.tip-method');
-  tipBlock.slideToggle();
+  var speed = 300;
+  if (tipBlock.is(':visible')) {
+    tipBlock.slideUp(speed);
+  } else {
+    tipBlock.slideDown(speed);
+  }
 });
